faa4606f3b32e1af68e7e2ddb7392165e7af37f6
foxsix <habibifauzan93@gmail.com>
2024-10-17 21:30:00 +0700

    ver.full

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Muhammad Fauzan Habibi'; // TODO: replace this
 
 const studentId = 'FS9680790'; // TODO: replace this
 
-const NetlifyDeployUrl = 'https://loquacious-tanuki-a63917.netlify.app'; // TODO: replace this
+const NetlifyDeployUrl = 'https://zippy-strudel-6a76d0.netlify.app/'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
