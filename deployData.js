ebff62273b590b842fb038f8e05bb9bf7363689f
foxsix <habibifauzan93@gmail.com>
2024-10-17 19:39:01 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Muhammad Fauzan Habibi'; // TODO: replace this
+
+const studentId = 'FS9680790'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://loquacious-tanuki-a63917.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
